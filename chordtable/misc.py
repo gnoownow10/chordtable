4173a6abe5b6a6ee8344c2ec8821b5a224bc8ad2
01moumooug <01moumooug@gmail.com>
2016-01-10 04:05:49 +0900

    add format_fudi

--- chordtable/misc.py
+++ chordtable/misc.py
@@ -1,3 +1,5 @@
+import socket
+
 def get_color(chordtable, pitch_class):
     r = chordtable.weights[pitch_class] / chordtable.max_weight
 
@@ -9,3 +11,9 @@ def get_color(chordtable, pitch_class):
 
 def format_color(red, green, blue):
     return '#{:02x}{:02x}{:02x}'.format(red, green, blue)
+
+def format_fudi(*values):
+    values = map(str, values)
+    values = (v.replace(' ', '\\ ') for v in values)
+    values = (v.encode('ascii') for v in values)
+    return b' '.join(values) + b';'
