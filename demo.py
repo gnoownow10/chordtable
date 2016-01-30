0609945fef81f72e525a9b5b3cadc5b2b1576ef5
01moumooug <01moumooug@gmail.com>
2016-01-31 02:31:17 +0900

    print weights to stdout

--- demo.py
+++ demo.py
@@ -2,25 +2,32 @@
 import socket
 import time
 from sys import exit, stdout, path
+from functools import partial
 
 from demo.composer import Composer
 from chordtable.chordtable import Chordtable
 from chordtable.misc import format_fudi
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('localhost', 7070))
+s.connect(('localhost', 8888))
 
 chord = Chordtable()
 crap_composer = Composer()
 
 while True:
     p, delta = chord.next_step()
-    for _ in range(0, 3):
-        if delta == 1:
-            crap_composer.weight(p)
-        elif delta == -1:
-            crap_composer.lighten(p)
+
+    if delta > 0 and not (chord.weights[p] < chord.max_weight):
+        continue
+
     chord = chord.modify((p, delta))
+    print(format_fudi(*chord.weights).decode('ascii'))
+
+    for _ in range(0, 3):
+        if delta > 0:
+            crap_composer.weight(p)
+        else:
+            crap_composer.lighten(p)
 
     for beat in range(0, 24):
         for voice, staff in enumerate(crap_composer.staves):
--- visualize.py
+++ visualize.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from chordtable.chordtable import Chordtable
 import chordtable.misc as misc
 
